ab1f914af2fc249259fa1a74b35187998a117960
Liam Carr <Liam@liamcarr.com>
2019-08-10 16:24:16 -0400

    initial commit

---
+++ src/main/java/com/liamcarr/dailyDrawJournal/DailyDrawJournalApplication.java
@@ -0,0 +1,13 @@
+package com.liamcarr.dailyDrawJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDrawJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyDrawJournalApplication.class, args);
+	}
+
+}
